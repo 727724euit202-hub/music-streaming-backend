86309858bca9969358f4e6431d1cb78725013eef
Rithish Nivash M <727724euit202@skcet.ac.in>
2026-02-01 20:45:12 +0530

    Initial commit - Song entity added

---
+++ project/src/main/java/com/song/project/SongUploadAndPlayApplication.java
@@ -0,0 +1,13 @@
+package com.song.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongUploadAndPlayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongUploadAndPlayApplication.class, args);
+	}
+
+}
